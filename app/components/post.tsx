868868df2fe01f7f5c201dc0b8e5b2158240b885
Sam McDonald <Sammcdonald828@gmail.com>
2025-04-01 12:57:35 -0400

    edit post

--- app/components/post.tsx
+++ app/components/post.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Form } from "react-router";
 
 type PostProps = {
@@ -11,28 +12,42 @@ type PostProps = {
 };
 
 export default function Post({ post, sessionUserId }: PostProps) {
+	const [isEditing, setIsEditing] = useState(false);
+
 	return (
 		<div key={post.id} className="card card-border bg-base-200 w-96 shadow-lg">
-			<a href={`/post/${post.id}`}>
-				<div className="card-body">
-					<h2 className="card-title">{post.title}</h2>
-					{sessionUserId === post.userId && (
-						<Form action="/deletePost" method="post">
-							<input type="hidden" name="postId" value={post.id} />
-							<button
-								type="submit"
-								className="mt-4 bg-red-500 text-white px-4 py-2 rounded"
-							>
-								delete
-							</button>
-						</Form>
-					)}
-					<p>{post.body}</p>
-					<div className="card-actions justify-end">
-						<button className="btn btn-primary">reply</button>
+			{isEditing ? (
+				<Form action="/editPost" method="post">
+					<div className="card-body">
+						<input className="card-title">{post.title}</input>
+						<textarea>{post.body}</textarea>
+						<div className="card-actions justify-end">
+							<button className="btn btn-primary">save</button>
+						</div>
 					</div>
-				</div>
-			</a>
+				</Form>
+			) : (
+				<a href={`/post/${post.id}`}>
+					<div className="card-body">
+						<h2 className="card-title">{post.title}</h2>
+						{sessionUserId === post.userId && (
+							<Form action="/deletePost" method="post">
+								<input type="hidden" name="postId" value={post.id} />
+								<button
+									type="submit"
+									className="mt-4 bg-red-500 text-white px-4 py-2 rounded"
+								>
+									delete
+								</button>
+							</Form>
+						)}
+						<p>{post.body}</p>
+						<div className="card-actions justify-end">
+							<button className="btn btn-primary">reply</button>
+						</div>
+					</div>
+				</a>
+			)}
 		</div>
 	);
 }
