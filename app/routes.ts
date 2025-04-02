0b2700d9bc7ec343ec269adc610309f3fb972ca3
Sam McDonald <Sammcdonald828@gmail.com>
2025-04-02 16:29:58 -0400

    moved auth related routes to auth folder

--- app/routes.ts
+++ app/routes.ts
@@ -2,9 +2,9 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
 	index("routes/home.tsx"),
-	route("login", "routes/login.tsx"),
-	route("join", "routes/join.tsx"),
-	route("logout", "routes/logout.tsx"),
+	route("login", "routes/auth/login.tsx"),
+	route("join", "routes/auth/join.tsx"),
+	route("logout", "routes/auth/logout.tsx"),
 	route("newPost", "routes/actions/newPost.tsx"),
 	route("editPost", "routes/actions/editPost.tsx"),
 	route("deletePost", "routes/actions/deletePost.tsx"),
